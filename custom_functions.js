26e130b9af3f202bf636f1c97e751ecf5f24478d
Bartłomiej Cywiński <bartlomiej.cywinski@poczta.onet.pl>
2023-07-17 16:00:09 +0200

    add greeting message

---
+++ custom_functions.js
@@ -0,0 +1,8 @@
+const name_nick = prompt("Podaj swoje imię:");
+
+
+if(name_nick){
+    alert(`Cześć ${name_nick}, witaj na mojej stronie`);
+} else {
+    alert('Cześć, witaj na mojej stronie');
+}
