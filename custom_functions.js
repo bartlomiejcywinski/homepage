3c69701517a0c9ec18f1c8f9209d5ec10690d600
Bartłomiej Cywiński <bartlomiej.cywinski@poczta.onet.pl>
2023-07-17 16:09:47 +0200

    add massage in console

--- custom_functions.js
+++ custom_functions.js
@@ -3,6 +3,8 @@ const name_nick = prompt("Podaj swoje imię:");
 
 if(name_nick){
     alert(`Cześć ${name_nick}, witaj na mojej stronie`);
+    console.log(`W tym sekretnym miejscu również Cię witam ${name_nick} :).`)
 } else {
     alert('Cześć, witaj na mojej stronie');
+    console.log('Cześć, witaj na mojej stronie');
 }
