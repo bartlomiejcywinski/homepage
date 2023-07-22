8e4abd0103bf54b182a9fae32dc6f33f4fa0b747
Bartłomiej Cywiński <bartlomiej.cywinski@poczta.onet.pl>
2023-07-22 09:55:00 +0200

    add new function
    - add content
    - add color end textAlign to new content

---
+++ custom_functions.js
@@ -0,0 +1,5 @@
+let getContainerJs = document.querySelector('.article__container--js');
+
+getContainerJs.innerHTML = 'Dodanie treści za pomocą skryptu';
+getContainerJs.style.color = 'violet';
+getContainerJs.style.textAlign = 'center';
