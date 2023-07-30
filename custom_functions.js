f100ee76eccbf24cfde297a9c1cd1dc25c30eddd
Bartłomiej Cywiński <bartlomiej.cywinski@poczta.onet.pl>
2023-07-30 10:43:05 +0200

    create greeting function

--- custom_functions.js
+++ custom_functions.js
@@ -3,3 +3,10 @@ let getContainerJs = document.querySelector('.article__container--js');
 getContainerJs.innerHTML = 'Dodanie treści za pomocą skryptu';
 getContainerJs.style.color = 'violet';
 getContainerJs.style.textAlign = 'center';
+
+function greeting(name, age){
+    const message = `Witaj ${name}. Masz ${age} lat.`;
+    console.log(message);
+}
+
+greeting('Bartek', 9999);
