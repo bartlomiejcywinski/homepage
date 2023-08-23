09c7668dbb994c0a1b8aac975ede4ac71607c141
Bartłomiej Cywiński <bartlomiej.cywinski@poczta.onet.pl>
2023-08-23 19:52:12 +0200

    add hamburger menu

--- custom_functions.js
+++ custom_functions.js
@@ -1,12 +1,22 @@
 let getContainerJs = document.querySelector('.article__container--js');
 
-getContainerJs.innerHTML = 'Dodanie treści za pomocą skryptu';
-getContainerJs.style.color = 'violet';
-getContainerJs.style.textAlign = 'center';
+if (getContainerJs) {
+    getContainerJs.innerHTML = 'Dodanie treści za pomocą skryptu';
+    getContainerJs.style.color = 'violet';
+    getContainerJs.style.textAlign = 'center';
+}
 
-function greeting(name, age){
+function greeting(name, age) {
     const message = `Witaj ${name}. Masz ${age} lat.`;
     console.log(message);
 }
 
 greeting('Bartek', 9999);
+
+const getMenuButton = document.querySelector('.hamburger--js');
+
+getMenuButton.addEventListener('click', (e) => {
+    const navigation = document.querySelector('.top-nav--js');
+
+    navigation.classList.toggle('open');
+})
